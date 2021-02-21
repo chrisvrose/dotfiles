7165681286c5ac5ca721bf2a9d3cf3c63a5bc6da
Atreya Bain <christophervrose@gmail.com>
2021-02-21 11:25:54 +0530

    Mention deps for later

---
+++ setup.sh
@@ -0,0 +1,7 @@
+test ! -d ~/.config/i3 && ln -s "$(pwd)/i3" ~/.config/i3
+
+test ! -d ~/.config/i3status && ln -s "$(pwd)/i3status" ~/.config/i3status
+
+test ! -d ~/.config/picom && ln -s "$(pwd)/picom" ~/.config/picom
+
+
