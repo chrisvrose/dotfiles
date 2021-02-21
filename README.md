e7cbeaef5d5356137d63f8e7cd558feea19113bd
Atreya Bain <christophervrose@gmail.com>
2021-02-21 19:47:29 +0530

    Add rofi

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # dotfiles
 Config files for my environment
+
+Wallpaper from: https://old.reddit.com/r/Art/comments/7ysmt2/lakeside_sunset_digital_8000x4500/
+
+
+# What do i use
+
+|**For**| **What**|
+|---|---|
+|Base|Manjaro(XFCE)|
+|Window Manager| `i3`|
+|Compositor|`picom`|
+|Application Launcher| ~~`dmenu`~~ `rofi`|
+|Color Schemes| `pywal`|
+|Wallpaper| `feh` (`pywal` uses it)|
+
+
+## What have I changed?
+### i3
+
+- Logout message
+- Use colors
+- `i3bar` invocation arguments
+- `picom` and `wal` invocation
+- Switch to `amixer` to control volume. `pactl` has no volume bounds
+
+### i3status
+
+- Added optional cpu usage
+- Network(s) show IP only if connected
+- No seconds
+
+### picom
+- Add transparency for rofi
--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+# i3
 test ! -d ~/.config/i3 && ln -s "$(pwd)/i3" ~/.config/i3
-
+# i3status
 test ! -d ~/.config/i3status && ln -s "$(pwd)/i3status" ~/.config/i3status
-
+# picom
 test ! -d ~/.config/picom && ln -s "$(pwd)/picom" ~/.config/picom
+# rofi
+test ! -d ~/.config/rofi && ln -s "$(pwd)/rofi" ~/.config/rofi
 
-# add nvm
+# wallpaper 
+mkdir -p ~/.config/wallpaper/
+test ! -f ~/.config/wallpaper/wallpaper.jpg && echo "Download to ~/.config/wallpaper/wallpaper.jpg: https://old.reddit.com/r/Art/comments/7ysmt2/lakeside_sunset_digital_8000x4500/"
 
-test ! -d ~/.nvm && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-
-
+# colorscheme generator
+wal -i ~/.config/wallpaper/wallpaper.jpg
 
