7652a1771ef84446ad4df669378cb0c9ce49cdd1
Atreya Bain <christophervrose@gmail.com>
2021-02-21 11:50:34 +0530

    Add nvm

--- setup.sh
+++ setup.sh
@@ -4,4 +4,9 @@ test ! -d ~/.config/i3status && ln -s "$(pwd)/i3status" ~/.config/i3status
 
 test ! -d ~/.config/picom && ln -s "$(pwd)/picom" ~/.config/picom
 
+# add nvm
+
+test ! -d ~/.nvm && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+
 
