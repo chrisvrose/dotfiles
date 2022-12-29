543ce3ed5725c9bd7135a58a4c546085e452175d
Atreya Bain <atreyabn@gmail.com>
2022-12-29 17:41:16 +0530

    [cleanup] trim to use sample i3 config

--- setup.sh
+++ setup.sh
@@ -3,25 +3,28 @@
 test ! -d ~/.config/i3 && ln -s "$(pwd)/i3" ~/.config/i3
 # i3status
 test ! -d ~/.config/i3status && ln -s "$(pwd)/i3status" ~/.config/i3status
-# picom
-test ! -d ~/.config/picom && ln -s "$(pwd)/picom" ~/.config/picom
-# rofi
-test ! -f ~/.config/wallpaper/wallpaper.jpg && echo "Download to ~/.config/wallpaper/wallpaper.jpg: https://old.reddit.com/r/Art/comments/7ysmt2/lakeside_sunset_digital_8000x4500/"
-# dunst
-test ! -d ~/.config/dunst && ln -s "$(pwd)/dunst" ~/.config/dunst
 
-test ! -d ~/.config/alacritty && ln -s "$(pwd)/alacritty" ~/.config/alacritty
+## May not need this anymore
+# picom
+#test ! -d ~/.config/picom && ln -s "$(pwd)/picom" ~/.config/picom
+# rofi
+#test ! -f ~/.config/wallpaper/wallpaper.jpg && echo "Download to ~/.config/wallpaper/wallpaper.jpg: https://old.reddit.com/r/Art/comments/7ysmt2/lakeside_sunset_digital_8000x4500/"
+# dunst
+#test ! -d ~/.config/dunst && ln -s "$(pwd)/dunst" ~/.config/dunst
+
+#test ! -d ~/.config/alacritty && ln -s "$(pwd)/alacritty" ~/.config/alacritty
 
 # wallpaper 
-mkdir -p ~/.config/wallpaper/
-test -f ~/.config/wallpaper/wallpaper.jpg && echo "I>Wallpaper exists, setting up"
-test ! -f ~/.config/wallpaper/wallpaper.jpg && echo "Download to ~/.config/wallpaper/wallpaper.jpg: https://old.reddit.com/r/Art/comments/7ysmt2/lakeside_sunset_digital_8000x4500/"
+# mkdir -p ~/.config/wallpaper/
+# test -f ~/.config/wallpaper/wallpaper.jpg && echo "I>Wallpaper exists, setting up"
+# test ! -f ~/.config/wallpaper/wallpaper.jpg && echo "Download to ~/.config/wallpaper/wallpaper.jpg: https://old.reddit.com/r/Art/comments/7ysmt2/lakeside_sunset_digital_8000x4500/"
 
 # colorscheme generator
-wal -i ~/.config/wallpaper/wallpaper.jpg
+# wal -i ~/.config/wallpaper/wallpaper.jpg
 
 
 
 # dont use wpg
 # test ! -f ~/.config/wallpaper/wallpaper.jpg && wpg -a ~/.config/wallpaper/wallpaper.jpg && wpg -i wallpaper.jpg ./wpg.json && wpg -s wallpaper.jpg
 
+echo "Done"
