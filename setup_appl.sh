ceff8e35e69b1590fca381613f9637634aae1111
Atreya Bain <atreyabn@gmail.com>
2022-12-29 16:56:51 +0530

    [general] remove app setup

--- setup_appl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Setup dependencies
-# https://www.reddit.com/r/Art/comments/7ysmt2/lakeside_sunset_digital_8000x4500/
-# https://i.imgur.com/E7Naj5o.jpg
-
-test ! -d ~/.nvm && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
