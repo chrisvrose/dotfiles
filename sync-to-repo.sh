d1fdd4ab8c6f189d5bd7ecd9434acc759805da83
Atreya Bain <atreyabain@gmail.com>
2025-11-30 09:51:47 +0000

    [add] rofi and xdg desktop portal

--- sync-to-repo.sh
+++ sync-to-repo.sh
@@ -2,3 +2,5 @@
 
 rsync -ap $@ ~/.config/zed/ ./zed
 rsync -ap $@ ~/.config/sway/ ./sway
+rsync -ap $@ ~/.config/xdg-desktop-portal-wlr/ ./xdg-desktop-portal-wlr
+rsync -ap $@ ~/.config/rofi/ ./rofi
